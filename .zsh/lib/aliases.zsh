25e82a0d12ef8967d94426dc75ddf710c151ebba
elm <elm@skweez.net>
2012-07-07 11:54:40 +0200

    Added initial zsh configs

---
+++ .zsh/lib/aliases.zsh
@@ -0,0 +1,11 @@
+alias ll='ls -la'
+alias lh='ls -lah'
+alias ...='cd ../..'
+
+alias s='su --preserve-environment root'
+
+alias homegit='git --git-dir=$HOME/.home.git --work-tree=$HOME'
+
+upgrade() {
+	su -c "apt-get update && apt-get dist-upgrade && apt-get autoclean && apt-get autoremove"
+}
